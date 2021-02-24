f25cd6fa42593a01687744b5c229d95e734c7463
PhollaDol <dolpholla168@gmail.com>
2021-02-24 09:50:03 +0700

    update home page

--- src/app/website/home-page/home-page.component.ts
+++ src/app/website/home-page/home-page.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  image: string = "https://images.pexels.com/photos/2132095/pexels-photo-2132095.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260";
+  image: string = "https://images.pexels.com/photos/5982764/pexels-photo-5982764.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260";
   constructor() { }
 
   ngOnInit() {
